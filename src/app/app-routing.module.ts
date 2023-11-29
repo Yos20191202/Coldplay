4a3d1775546ff1a76dbc9a540165722173c6bddb
Yos20191202 <yossb201@gmail.com>
2023-11-29 12:44:08 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/InicioComponent';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { MusicaComponent } from './musica/musica.component';
+import { ConciertoComponent } from './concierto/concierto.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ContactosComponent } from './contactos/contactos.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: InicioComponent, },
+  { path: 'acerca-de', component: AcercaDeComponent },
+  { path: 'musica', component: MusicaComponent },
+  { path:'concierto', component: ConciertoComponent },
+  { path:'productos', component: ProductosComponent },
+  { path:'contactos', component: ContactosComponent },
+  { path:'login', component: LoginComponent },
+  { path:'registro', component:RegistroComponent}
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,43 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/InicioComponent';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { MusicaComponent } from './musica/musica.component';
+import { ConciertoComponent } from './concierto/concierto.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ContactosComponent } from './contactos/contactos.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AcercaDeComponent,
+    MusicaComponent,
+    ConciertoComponent,
+    ProductosComponent,
+    ContactosComponent,
+    CarouselComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: environment.production,
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
   providers: [],
   bootstrap: [AppComponent]
 })
